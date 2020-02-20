795443c396302ae3129a32b5310bd880b1802772
nadav <naweiss@g.jct.ac.il>
2020-02-20 22:34:29 +0200

    Moved close_on_error to new module

--- connection/socket.py
+++ connection/socket.py
@@ -1,20 +1,9 @@
 import websockets
 import logging
 
-from websockets.exceptions import ConnectionClosed
+from async_utils import close_on_error
 
 logger = logging.getLogger(__name__)
-
-
-def close_on_error(function):
-    async def wrapper(self, *args, **kwargs):
-        try:
-            return await function(self, *args, **kwargs)
-        except ConnectionClosed:
-            logger.warning('Error while in {}, closing connection'.format(function.__name__))
-            await self.close()
-
-    return wrapper
 
 
 class PythonAnyewhereSocket(object):
--- terminal/console.py
+++ terminal/console.py
@@ -1,20 +1,10 @@
 import asyncio
 import logging
 
+from async_utils import close_on_error
 from connection.codec import UnicodeCodec
 
 logger = logging.getLogger(__name__)
-
-
-def close_on_error(function):
-    async def wrapper(self, *args, **kwargs):
-        try:
-            return await function(self, *args, **kwargs)
-        except:
-            logger.warning('Error while in {}, closing connection'.format(function.__name__))
-            await self.close()
-
-    return wrapper
 
 
 class PythonAnywhereConsole(object):
