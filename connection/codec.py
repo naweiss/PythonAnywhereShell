a153674295a9c6ce467dc54f2d4a8ac54051109f
nadav <naweiss@g.jct.ac.il>
2020-03-03 22:02:16 +0200

    Bug fix - support for emojis

--- connection/codec.py
+++ connection/codec.py
@@ -1,8 +1,13 @@
 class UnicodeCodec(object):
     @staticmethod
     def encode(data):
-        return data.encode('unicode_escape').decode('utf-8')
+        return data.encode('unicode_escape').decode('utf-8').replace(r'\x', r'\u00')
 
     @staticmethod
     def decode(data):
-        return bytes(data, 'utf-8').decode('unicode_escape')
+        unescaped_unicode = data.encode('utf-8').decode('unicode_escape')
+        try:
+            unescaped_unicode.encode()
+            return unescaped_unicode
+        except UnicodeEncodeError:
+            return unescaped_unicode.encode('utf-16', 'surrogatepass').decode('utf-16')
