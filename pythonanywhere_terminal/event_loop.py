d3b449abbd4673cb6e302898543a305b23fc949d
nadav <naweiss@g.jct.ac.il>
2020-11-19 19:14:10 +0200

    Implemented auto close

--- pythonanywhere_terminal/event_loop.py
+++ pythonanywhere_terminal/event_loop.py
@@ -9,25 +9,20 @@ from pythonanywhere_terminal.terminal.raw import RawTerminal
 logger = logging.getLogger(__name__)
 
 
-async def _open_connection(session_id, console_id, is_windowed=False):
+async def _open_connection(session_id, console_id, is_windowed=False, auto_close=False):
     socket = await PythonAnyWhereSocket.connect(session_id, console_id)
 
     terminal = WindowedTerminal() if is_windowed else RawTerminal()
-    console = PythonAnywhereConsole(terminal, socket)
-
-    tasks = [
-        asyncio.ensure_future(console.write_loop()),
-        asyncio.ensure_future(console.read_loop()),
-        asyncio.ensure_future(console.change_window_size_loop()),
-    ]
-    await asyncio.gather(*tasks)
+    console = PythonAnywhereConsole(terminal, socket, auto_close)
+    await console.start()
 
 
-def start_terminal(client, executable, is_windowed=False):
+def start_terminal(client, executable, is_windowed=False, auto_close=False):
     console = client.get_console_instance(executable)
 
     loop = asyncio.get_event_loop()
     main_task = asyncio.ensure_future(_open_connection(session_id=client.get_cookie('sessionid'),
                                                        console_id=console['id'],
-                                                       is_windowed=is_windowed))
+                                                       is_windowed=is_windowed,
+                                                       auto_close=auto_close))
     loop.run_until_complete(main_task)
--- pythonanywhere_terminal/terminal/console.py
+++ pythonanywhere_terminal/terminal/console.py
@@ -7,27 +7,30 @@ from pythonanywhere_terminal.connection.codec import UnicodeCodec
 logger = logging.getLogger(__name__)
 
 
-class PythonAnywhereConsole(object):
-    CTRL_SLASH = '\x1f'
-
+class AbstractConsole(object):
     def __init__(self, terminal, socket):
         self.terminal = terminal
         self.socket = socket
         self.window_size = None
 
+    async def start(self):
+        await asyncio.gather(
+            asyncio.ensure_future(self.write_loop()),
+            asyncio.ensure_future(self.read_loop()),
+            asyncio.ensure_future(self.change_window_size_loop())
+        )
+
     async def close(self):
-        self.terminal.close()
         if self.socket.is_connected():
             await self.socket.close()
+        self.terminal.close()
 
     @close_on_error
     async def change_window_size_loop(self):
         while self.socket.is_connected():
-            window_size = self.terminal.get_window_size()
-            if window_size != self.window_size:
-                width, height = window_size
-                await self.socket.change_window_size(width, height)
-                self.window_size = window_size
+            width, height = self.terminal.get_window_size()
+            if (width, height) != self.window_size:
+                await self.handle_window_size_change(width, height)
             await asyncio.sleep(0)
 
     @close_on_error
@@ -35,15 +38,49 @@ class PythonAnywhereConsole(object):
         while self.socket.is_connected():
             char = self.terminal.read()
             if char is not None:
-                if char == self.CTRL_SLASH:
-                    await self.socket.close()
-                await self.socket.send(UnicodeCodec.encode(char))
+                await self.handle_input(char)
             await asyncio.sleep(0)
 
     @close_on_error
     async def write_loop(self):
         while self.socket.is_connected():
-            data = await self.socket.recv()
-            if data is not None:
-                self.terminal.write(UnicodeCodec.decode(data))
+            raw_data = await self.socket.recv()
+            if raw_data is not None:
+                await self.handle_output(raw_data)
             await asyncio.sleep(0)
+
+    def handle_window_size_change(self, width, height):
+        raise NotImplementedError()
+
+    def handle_output(self, raw_data):
+        raise NotImplementedError()
+
+    def handle_input(self, char):
+        raise NotImplementedError()
+
+
+class PythonAnywhereConsole(AbstractConsole):
+    CLOSE_PHRASE = '\r\nConsole closed.'
+    CTRL_SLASH = '\x1f'
+
+    def __init__(self, terminal, socket, auto_close=False):
+        super().__init__(terminal, socket)
+        self.auto_close = auto_close
+
+    async def handle_window_size_change(self, width, height):
+        await self.socket.change_window_size(width, height)
+        self.window_size = (width, height)
+
+    async def handle_output(self, raw_data):
+        data = UnicodeCodec.decode(raw_data)
+
+        if self.auto_close and data == self.CLOSE_PHRASE:
+            await self.close()
+        else:
+            self.terminal.write(data)
+
+    async def handle_input(self, data):
+        if data == self.CTRL_SLASH:
+            await self.close()
+        else:
+            await self.socket.send(UnicodeCodec.encode(data))
